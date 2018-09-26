a6e542319869f72c61bc47ba6e4281b04960452a
yuya-bakuretsu <yuuya_17n1101244@nnn.ed.jp>
2018-09-26 11:57:01 +0900

    空欄のバリデーション

--- pomodoro.js
+++ pomodoro.js
@@ -1,28 +1,36 @@
-
-
 $(function () {
     "use strict";
     var timer = document.getElementById("timer_zone");
-    var nowTime;
     var limit;
     var flag = 0;
-    var working = 5;
-    var rest = 3;
-    var breaking = rest + 8;
     var alarm;
+    var timerTime = [];
     $('#start').click(function () {
+        var timeValue = ["workingTime", "restTime", "breakingTime"]
+        var e = 0;
+        for (let i = 0; i < timeValue.length; i++) {
+            timerTime.push(document.getElementById(timeValue[i]).value);
+            if (timerTime[i] == "") {
+                e++;
+            }
+        }
+
         var confirmed = confirm("ポモドーロタイマーを発動します。準備はよろしいですか?")
+        if(e != 0){
+            alert("未入力の欄があります。確認して入力してください");
+            confirmed = false
+        }
         if (confirmed) {
             changeTurn();
             this.disabled = true
         }
     })
 
-    $('#stop').click(function (){
+    $('#stop').click(function () {
         start.disabled = false;
         flag = 0;
         clearInterval(alarm);
-        timer.innerHTML = "0分0秒"; 
+        timer.innerHTML = "0分0秒";
     })
 
     function changeTurn() {
@@ -31,34 +39,29 @@ $(function () {
         switch (flag % 8) {
             case 0:
                 alert("長休憩の時間です。休んでください");
-                rotate(breaking);
+                rotate(timerTime[2] * 60);
                 break;
 
             case 2:
             case 4:
             case 6:
                 alert("休憩時間です。5分休憩してください");
-                rotate(rest);
+                rotate(timerTime[1] * 60);
                 break;
 
             default:
                 alert("仕事時間です。頑張って仕事してください。");
-                rotate(working);
+                rotate(timerTime[0] * 60);
                 break;
         }
-    }
-
-    function getSecond() {
-        var current = new Date();
-        nowTime = current.getTime();
-        return nowTime;
     }
 
     function rotate(turn) {
         var now = new Date();
         var sec = now.getTime();
         function updateTimer() {
-            getSecond();
+            var current = new Date();
+            var nowTime = current.getTime();
             var later = nowTime - sec;
             limit = Math.floor(turn - (later / 1000));
             timer.innerHTML = Math.floor(limit / 60) + "分" + Math.floor(limit % 60) + "秒";
